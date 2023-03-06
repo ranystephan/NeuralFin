162a7a731ada5bf629d0d1cc52d02b9b6b760e0a
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-06 14:37:43 +0200

    gitignore mod

--- NeuralFin-Django/NeuralFin_Django/settings.py
+++ NeuralFin-Django/NeuralFin_Django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lc^ftp^6z%nf8qrc(c$bbsm54t8c4ea(u$u-l+y3f5c+gx*su8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,9 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'AwT5yAbz5stCTDMmjrww',
-        'HOST': 'containers-us-west-102.railway.app',
-        'PORT': '7857',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
