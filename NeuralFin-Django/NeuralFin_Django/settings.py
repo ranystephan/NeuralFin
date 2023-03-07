cf66ffca6d3845d783d35a21c34a09e70a725eb2
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-07 23:01:48 +0200

    init of rest api (drf)

--- NeuralFin-Django/NeuralFin_Django/settings.py
+++ NeuralFin-Django/NeuralFin_Django/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
