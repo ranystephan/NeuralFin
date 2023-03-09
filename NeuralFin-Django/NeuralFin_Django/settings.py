bd924458e41818d8414f28f88cfe87781cf3ec5f
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-09 21:02:05 +0200

    staticfiles

--- NeuralFin-Django/NeuralFin_Django/settings.py
+++ NeuralFin-Django/NeuralFin_Django/settings.py
@@ -21,6 +21,8 @@ import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
+from django.conf import settings
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -36,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
