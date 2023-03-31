26d6d8fe2f7de46d4ab46fe94bc1fb7c34aa90ab
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-31 02:54:09 +0200

    solved stock models issue

--- NeuralFin-Django/stocks/views.py
+++ NeuralFin-Django/stocks/views.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Stock
+from .serializers import StockSerializer
 
 
 def get_data_stock(symbol):
