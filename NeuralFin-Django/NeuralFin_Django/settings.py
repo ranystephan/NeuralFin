0f58c5fc640050d2a89b761edde4c9ac5940dca1
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-04-20 18:35:09 +0300

    cors allowed origins update

--- NeuralFin-Django/NeuralFin_Django/settings.py
+++ NeuralFin-Django/NeuralFin_Django/settings.py
@@ -159,5 +159,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.User' 
-CORS_ORIGIN_ALLOW_ALL = True # to allow all origins
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True # to allow cookies to be sent across domains
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://neuralfin.xyz",
+]
+    
