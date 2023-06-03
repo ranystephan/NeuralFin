e8db45f18df37b8184c851037e4245311de5e3b7
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-06-03 04:32:04 +0300

    username generation  + put function for user

--- NeuralFin-Django/users/tests.py
+++ NeuralFin-Django/users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APIClient
+
+from .models import User
+
+client = APIClient()
+
+response = client.post('/register/', {'name': 'John Doe', 'email': 'john@example.com', 'password': 'password123'})
+assert response.status_code == 200
+
+
+response = client.post('/login/', {'email': 'john@example.com', 'password': 'password123'})
+assert response.status_code == 200
+access_token = response.data['jwt']
+
+
+client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+response = client.put('/update_profile/', {'username': 'new_username', 'email': 'new_email@example.com'}, format='json')
+assert response.status_code == 200
