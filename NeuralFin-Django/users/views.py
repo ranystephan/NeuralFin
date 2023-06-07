db72751bae902a01442540a6a16512c069c52148
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-06-08 00:09:22 +0300

    last test didn't work, back to normal

--- NeuralFin-Django/users/views.py
+++ NeuralFin-Django/users/views.py
@@ -18,6 +18,7 @@ def generate_unique_username(name):
     return username
 
 
+
 class RegisterView(APIView):
     def post(self, request):
         data = request.data.copy()
@@ -26,6 +27,7 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
 
 
 class LoginView(APIView):
@@ -45,13 +47,7 @@ class LoginView(APIView):
         access_token = str(refresh.access_token)
 
         response = Response()
-        response.set_cookie(
-            key='jwt',
-            value=access_token,
-            httponly=True,
-            secure=True,
-            samesite='None',
-        )
+        response.set_cookie(key='jwt', value=access_token, httponly=True, samesite='None', secure=True) #, httponly=True, samesite='None', secure=True
         response.data = {
             'jwt': access_token
         }
@@ -59,28 +55,51 @@ class LoginView(APIView):
 
 
 class UserView(APIView):
-    authentication_classes = [JWTAuthentication]
 
     def get(self, request):
-        user = request.user
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            jwt_authentication = JWTAuthentication()
+            validated_token = jwt_authentication.get_validated_token(token)
+            user = jwt_authentication.get_user(validated_token)
+        except InvalidToken:
+            raise AuthenticationFailed('Unauthenticated!')
+
         serializer = UserSerializer(user)
         return Response(serializer.data)
-
+    
     def put(self, request):
-        user = request.user
+        token = request.COOKIES.get('jwt')
+        
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        
+        try:
+            jwt_authentication = JWTAuthentication()
+            validated_token = jwt_authentication.get_validated_token(token)
+            request.user = jwt_authentication.get_user(validated_token)
+        except InvalidToken:
+            raise AuthenticationFailed('Unauthenticated!')
+        
         data = request.data.copy()
         data['username'] = generate_unique_username(data['username'])
-        serializer = UserSerializer(instance=user, data=data, partial=True)
+        serializer = UserSerializer(instance=request.user, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
+    
 
 class LogoutView(APIView):
     def post(self, request):
         response = Response()
-        response.delete_cookie('jwt', secure=True, samesite='None')
+        response.delete_cookie('jwt', samesite='None')
         response.data = {
             'message': 'success'
         }
         return response
+
+
